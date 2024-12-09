e689a280a13430f2202b9fc1d3f9a2fa44d04032
Jason Enns <enns@wave-distribution.de>
2024-12-09 16:32:49 +0100

    something

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,6 @@ import MediaRecorder from 'vue-media-recorder';
 import VQrcode from 'qrcode-vuejs';
 
 
-document.querySelector('meta[name="permissions-policy"]')?.setAttribute('content', 'camera=(self https://amongus.inforgeserver.de)');
-
 const app = createApp(App)
 app.use(VueCookies)
 app.use(router)
